45255a9ca620f36c82084a50b08c44ef80debf73
Milton Navarro <milton.j.navarro.p@gmail.com>
2020-10-04 17:47:29 -0600

    Add project files.

---
+++ WinformCustomersReport/frm/frmCustomersReport.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WinformCustomersReport.frm
+{
+    partial class frmCustomersReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExportToPDF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExportToPDF
+            // 
+            this.btnExportToPDF.Location = new System.Drawing.Point(280, 130);
+            this.btnExportToPDF.Name = "btnExportToPDF";
+            this.btnExportToPDF.Size = new System.Drawing.Size(221, 102);
+            this.btnExportToPDF.TabIndex = 1;
+            this.btnExportToPDF.Text = "Export customers to PDF";
+            this.btnExportToPDF.UseVisualStyleBackColor = true;
+            this.btnExportToPDF.Click += new System.EventHandler(this.btnExportToPDF_Click);
+            // 
+            // frmCustomersReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExportToPDF);
+            this.Name = "frmCustomersReport";
+            this.Text = "frmCustomersReport";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExportToPDF;
+    }
+}
---
+++ WinformCustomersReport/frm/frmCustomersReport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformCustomersReport.frm
+{
+    public partial class frmCustomersReport : Form
+    {
+        public frmCustomersReport()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExportToPDF_Click(object sender, EventArgs e)
+        {
+            rpt.rptCustomers report = new rpt.rptCustomers();
+            report.RequestParameters = false;
+            report.ExportToPdf("Customers.pdf"); //export to pdf on the debug folder
+        }
+    }
+}
